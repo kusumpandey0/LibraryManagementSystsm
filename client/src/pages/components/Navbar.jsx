6b530ca1c82d9c620f11c36856b316c95253b01d
Kusum Pandey <kkuussuummss@gmail.com>
2025-02-14 02:11:02 +0545

    third

--- client/src/pages/components/Navbar.jsx
+++ client/src/pages/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
@@ -18,7 +18,7 @@ const Navbar = () => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               BookStore
             </span>
-          </a>
+          </Link>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <Link to="/addBook">
               <button
